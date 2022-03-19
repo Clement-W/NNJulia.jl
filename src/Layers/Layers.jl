96ef7165e8a6c5759b1d46091991ecb466ad79ff
Clément Weinreich <clement@weinreich.fr>
2022-03-19 19:21:54 +0100

    Add convolution struct (to implement later)

--- src/Layers/Layers.jl
+++ src/Layers/Layers.jl
@@ -3,12 +3,13 @@ module Layers
 abstract type AbstractLayer end
 abstract type AbstractModel end
 
-export Dense, Sequential
+export Dense, Sequential, Convolution
 
 # two dots to import from parent module
 using ..Autodiff
 
 include("Dense.jl")
 include("Sequential.jl")
+include("Convolution.jl")
 
 end
--- src/NNJulia.jl
+++ src/NNJulia.jl
@@ -5,7 +5,7 @@ export AbstractTensor, Tensor, TensorDependency, zero_grad!, backward!
 using .Autodiff
 
 include("Layers/Layers.jl")
-export Dense, Sequential, zero_grad!
+export Dense, Sequential, zero_grad!,Convolution
 using .Layers
 
 end
