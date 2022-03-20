d6705d1d38c55b8c8e2b2ee5b050189d7aeea00f
Clément Weinreich <clement@weinreich.fr>
2022-03-20 15:44:42 +0100

    Add comments

--- src/Optimisers/GradientDescent.jl
+++ src/Optimisers/GradientDescent.jl
@@ -1,11 +1,14 @@
 using .Layers
 
+# struct storing the learning rate for gradient descent optimiser
 struct GradientDescent <: AbstractOptimiser
     lr::Float64
 end
 
+# default constructor, initialising the learning rate at 0.1
 GradientDescent() = GradientDescent(0.1)
 
+# update the parameters of a model with gradient descent algorithm 
 function update!(opt::GradientDescent, model::AbstractModel)
     for p in parameters(model)
         p.data = p.data .- opt.lr .* p.gradient
