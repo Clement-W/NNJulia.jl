a0f9af1091d2c77065a4f650ab44b0fd35d73b4f
Clément Weinreich <clement@weinreich.fr>
2022-04-05 15:05:40 +0200

    Add documentation to loss

--- src/Loss/Loss.jl
+++ src/Loss/Loss.jl
@@ -4,18 +4,43 @@ export AbstractLoss, MSE, BinaryCrossentropy, compute_loss
 
 using ..Autodiff
 
+"""
+    AbstractLoss
+
+Every loss struct is a subtype of AbstractLoss
+"""
 abstract type AbstractLoss end
 
+"""
+    MSE
+
+Represents the Mean Squared Error : L(y,y^i)=∑(y-y^i)^2
+"""
 struct MSE <: AbstractLoss end
 
+
+"""
+    BinaryCrossentropy
+
+Represents the Binary crossentropy error function
+"""
 struct BinaryCrossentropy <: AbstractLoss end
 
+
+"""
+    compute_loss(lossF::MSE, predicted::Tensor, target::Union{Tensor,AbstractArray,Float64,Int64})
+    compute_loss(lossF::BinaryCrossentropy, predicted::Tensor, target::Union{Tensor,AbstractArray,Float64,Int64})
+
+Compute the loss according to the lossF given.
+"""
+# Compute loss for MSE
 function compute_loss(lossF::MSE, predicted::Tensor, target::Union{Tensor,AbstractArray,Float64,Int64})
     #L(y,y^i)=∑(y−y^​i​)^2
     errors = predicted .- target
     return sum(errors .* errors)
 end
 
+# Compute loss for BinaryCrossentropy
 function compute_loss(lossF::BinaryCrossentropy, predicted::Tensor, target::Union{Tensor,AbstractArray,Float64,Int64})
     # https://peltarion.com/knowledge-center/documentation/modeling-view/build-an-ai-model/loss-functions/binary-crossentropy
     f = -1 / length(predicted)
@@ -23,7 +48,7 @@ function compute_loss(lossF::BinaryCrossentropy, predicted::Tensor, target::Unio
     part2 = log(1 .- predicted)
     # For an unknown reason, it does not return a Tensor if I don't put part1 and part2 into 2 different variables
     # (1 .- target) .* log(1 .- predicted) don't return a tensor
-    # but part1 .* part2 do return a tenor
+    # but part1 .* part2 do return a tensor
     res = f .* sum((target .* log(predicted)) .+ (part1 .* part2))
     return res
 end
