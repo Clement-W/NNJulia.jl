7e4977d0ebfd2352fc78d467f9973eeae126ac26
Clément Weinreich <clement@weinreich.fr>
2022-04-22 21:07:37 +0200

    Update repo name in docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,7 @@ push!(LOAD_PATH, "../src/")
 makedocs(modules=[NNJulia], sitename="NNJulia")
 
 deploydocs(
-    repo="github.com/Clement-W/Julia-DeepLearning.git",
+    repo="github.com/Clement-W/NNJulia.jl.git",
     devbranch="gh-pages"
 )
 
